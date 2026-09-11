eb26cd2e2de1fe1baa19247ce6dcb66120c62544
SHAWM17 <saumiyahya17@gmail.com>
2026-09-11 09:55:25 +0700

    percobaan3

--- jobsheet3/MotorEncapsulation/Motor.java
+++ jobsheet3/MotorEncapsulation/Motor.java
@@ -1,8 +1,8 @@
 package jobsheet3.MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
 
     public void printStatus(){
         if (kontakOn == true){
@@ -12,5 +12,34 @@ public class Motor {
             System.out.println("Kontak Off");
         }
         System.out.println("Kecepatan " + kecepatan + "\n");
+    }
+
+    public void nyalakanMesin(){
+        kontakOn = true;
+    }
+
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan(){
+        if (kontakOn == true){
+            if (kecepatan < 100){
+                kecepatan += 5;
+            } else {
+                System.out.println("Tidak bisa menambah keceptan, Kecepatan sudah mencapai batas");
+            }
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin off! \n");
+        }
+    }
+
+    public void kurangiKecepatan(){
+        if (kontakOn == true){
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin off! \n");
+        }
     }
 }
