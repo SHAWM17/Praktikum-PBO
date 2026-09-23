1de51c94c75c75a9efb7daba7375934a9ac0e8dc
SHAWM17 <saumiyahya17@gmail.com>
2026-09-23 23:38:26 +0700

    percobaan4

---
+++ jobsheet4/254107020137.relasiClass.percobaan4/Kursi.java
@@ -0,0 +1,25 @@
+public class Kursi {
+    private String nomor;
+    private Penumpang penumpang;
+
+    public Kursi(String nomor) {
+        this.nomor = nomor;
+    }
+
+    public void setPenumpang(Penumpang penumpang) {
+        this.penumpang = penumpang;
+    }
+
+    public Penumpang getPenumpang() {
+        return penumpang;
+    }
+
+    public String info() {
+        String info = "";
+        info += "Nomor: " + nomor + "\n";
+        if (this.penumpang != null) {
+            info += "Penumpang:\n" + penumpang.info();
+        }
+        return info;
+    }
+}
